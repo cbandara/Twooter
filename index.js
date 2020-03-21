8a23af083d42a57e7b50fe74bfd2c015c4712e9e
Charutha Bandara <charuthabandara@gmail.com>
2020-03-20 23:56:47 -0400

    authorization authentication and messages routes

--- index.js
+++ index.js
@@ -5,14 +5,31 @@ const cors = require('cors');
 const bodyParser = require('body-parser')
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
 
 const PORT = 8081;
 
 app.use(cors())
 app.use(bodyParser.json());
 
-app.use("/api/auth", authRoutes);
 // All Routes will go here
+app.use("/api/auth", authRoutes);
+app.use("/api/users/:id/messages", loginRequired, ensureCorrectUser, messagesRoutes);
+
+app.get("/api/messages", loginRequired, async function (req, res, next) {
+  try {
+    let messages = await db.Message.find()
+      .sort({ createdAt })
+      .populate("user", {
+        username: true,
+        profileImageUrl: true
+      });
+    return res.status(200).json(messages);
+  } catch (error) {
+    return next(error);
+  }
+})
 
 app.use(function (req, res, next) {
   let err = new Error("Not Found")
